066f23222bb2626c8ab26e86c68a069a0826b6d8
sonal-jk <sonal1024jk@gmail.com>
2023-04-11 22:01:53 +0530

    commit and push

--- module-5/js/script.js
+++ module-5/js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      insertHtml("main-content",homeHtmlToInsertIntoMainPage)
+      insertHtml("#main-content",homeHtmlToInsertIntoMainPage)
 
        
 
